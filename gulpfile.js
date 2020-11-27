760b5484ad24164339a86b90cd97c43fc2b3a0f1
kurudrive <kurudrive@gmail.com>
2020-11-27 12:02:07 +0900

    bootstrap読み込み

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,15 @@ var sass = require('gulp-sass');
 // ファイル結合
 var concat = require("gulp-concat");
 
+const aliases = require('gulp-style-aliases')
+
 // error handling
 // var plumber = require('gulp-plumber');
 gulp.task('sass', function(done) {
-    gulp.src('./theme/_scss/*.scss')
+	gulp.src('./theme/_scss/*.scss')
+		.pipe(aliases({
+			"@bootstrap": "./node_modules/bootstrap/scss"
+		}))
         .pipe(sass())
 		.pipe(gulp.dest('./theme/css/'));
 		done();
---
+++ sample.md
@@ -0,0 +1,32 @@
+---
+marp: true
+---
+<!-- 
+theme: my_theme
+size: 16:9
+paginate: true
+style: |
+_paginate: false 
+-->
+<!-- _class: title -->
+<!-- Scoped style -->
+<style scoped>
+  /*
+section{
+  background: yellow;
+}
+*/
+</style>
+
+---
+
+# スライドサンプル
+
+---
+
+## アラート
+
+<div class="alert alert-info">テスト</div>
+<div class="alert alert-success">テスト</div>
+<div class="alert alert-warning">テスト</div>
+<div class="alert alert-danger">テスト</div>
